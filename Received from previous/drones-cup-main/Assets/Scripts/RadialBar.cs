51a3858823f51af6c0f8d94bb23f5a4c1a05bea7
OrangeFires <86798693+OrangeFires@users.noreply.github.com>
2026-04-21 11:55:51 +0200

    Added feature meter
    Added feature meter

---
+++ Received from previous/drones-cup-main/Assets/Scripts/RadialBar.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
+
+public class RadialBar : MonoBehaviour
+{
+    public float MaxValue;
+
+    public float currentValue;
+
+    void Start()
+    {
+        
+    }
+
+    /// <summary>
+    /// Changes the value of the radialbar with float amount
+    /// </summary>
+    void ChangeValue(float amount)
+    {
+        currentValue += amount;
+    }
+
+    /// <summary>
+    /// Sets the value of the radialbar to float amount
+    /// </summary>
+    void SetValue(float value)
+    {
+        currentValue = value;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float radialValue = currentValue / MaxValue;
+        this.gameObject.GetComponent<UnityEngine.UI.Image>().fillAmount = radialValue;
+    }
+}
